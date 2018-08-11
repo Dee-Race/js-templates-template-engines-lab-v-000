390c87444c392588cce5b58f18900a152a411998
Dee Race <deerace12@gmail.com>
2018-08-11 19:31:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function createPost(){
+  // create template functions
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let postTemplate = _.template(document.getElementById("post-template").innerHTML);
   let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
